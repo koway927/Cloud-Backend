4551952d5461d4c8b474df800411a95c63cdaae1
Yuting Wang <yw4554@nyu.edu>
2024-03-30 17:19:33 -0500

    starting from the beginning

--- CloudNetworkingCourse/src/main/java/com/networking/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.networking.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.networking.demo.Model.entity.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
