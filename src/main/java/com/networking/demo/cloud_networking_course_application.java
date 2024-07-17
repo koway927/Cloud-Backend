357a63bba07f9623be4a2a439b7cb904f5562baf
KO Wai Ho <raywhko@inno-access.hk>
2024-07-17 16:37:43 +0800

    Testing CICD

--- src/main/java/com/networking/demo/cloud_networking_course_application.java
+++ src/main/java/com/networking/demo/cloud_networking_course_application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class cloud_networking_course_application {
 
     public static void main(String[] args) {
+        System.out.println("Starting Cloud Networking Course Application backend...");
         SpringApplication.run(cloud_networking_course_application.class, args);
     }
 
